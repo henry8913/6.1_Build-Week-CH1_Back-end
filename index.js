9930b8f9f82ad99cd6d81d710eb80e62975793e7
“henry8913” <tarantola3.0>
2025-05-11 23:07:55 +0200

    0.1.6

---
+++ index.js
@@ -0,0 +1,21 @@
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 2025;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Info route
+app.get("/info", (req, res) => {
+  res.status(200).send("Benvenuto! Il progetto è attualmente in fase di sviluppo. Presto sarà disponibile online con tutte le sue funzionalità.");
+});
+
+// Avvio del server
+app.listen(PORT, "localhost", () => {
+  console.log(`Server in esecuzione sulla porta ${PORT}`);
+});
