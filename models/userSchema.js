16bb3452c8af80e895c07c36b012c2bd07a50b84
“henry8913” <tarantola3.0>
2025-05-16 13:38:13 +0200

    0.3.9

--- models/userSchema.js
+++ models/userSchema.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     lastName: { type: String, required: true },
     username: { type: String, required: true, unique: true },
-    address: { type: String, default: '' },
+    address: { type: String, required: false },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     avatar: { type: String, default: 'https://ui-avatars.com/api/?name=User&background=random' },
